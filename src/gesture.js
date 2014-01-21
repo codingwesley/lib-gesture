23f848162e63f1fe29e4e7e250127ef68b545458
勾股 <jinjiang.zjj@alibaba-inc.com>
2014-01-22 00:19:17 +0800

    code formated a little bit

--- src/gesture.js
+++ src/gesture.js
@@ -1,22 +1,39 @@
 ;(function(win, lib, undef) {
 
+'use strict';
+
 var doc = win.document,
     docEl = doc.documentElement,
     slice = Array.prototype.slice,
     gestures = {}, lastTap = null
     ;
 
-function getCommonAncestor (el1, el2) {
+/**
+ * 找到两个结点共同的最小根结点
+ * 如果跟结点不存在，则返回null
+ * 
+ * @param  {Element} el1 第一个结点
+ * @param  {Element} el2 第二个结点
+ * @return {Element}     根结点
+ */
+function getCommonAncestor(el1, el2) {
     var el = el1;
     while (el) {
         if (el.contains(el2) || el == el2) {
             return el;
         }
         el = el.parentNode;
-    }    
+    }
     return null;
 }
 
+/**
+ * 触发一个事件
+ * 
+ * @param  {Element} element 目标结点
+ * @param  {string}  type    事件类型
+ * @param  {object}  extra   对事件对象的扩展
+ */
 function fireEvent(element, type, extra) {
     var event = doc.createEvent('HTMLEvents');
     event.initEvent(type, true, true);
@@ -30,6 +47,23 @@ function fireEvent(element, type, extra) {
     element.dispatchEvent(event);
 }
 
+/**
+ * 计算变换效果
+ * 假设坐标系上有4个点ABCD
+ * > 旋转：从AB旋转到CD的角度
+ * > 缩放：从AB长度变换到CD长度的比例
+ * > 位移：从A点位移到C点的横纵位移
+ * 
+ * @param  {number} x1 上述第1个点的横坐标
+ * @param  {number} y1 上述第1个点的纵坐标
+ * @param  {number} x2 上述第2个点的横坐标
+ * @param  {number} y2 上述第2个点的纵坐标
+ * @param  {number} x3 上述第3个点的横坐标
+ * @param  {number} y3 上述第3个点的纵坐标
+ * @param  {number} x4 上述第4个点的横坐标
+ * @param  {number} y4 上述第4个点的纵坐标
+ * @return {object}    变换效果，形如{rotate, scale, translate[2], matrix[3][3]}
+ */
 function calc(x1, y1, x2, y2, x3, y3, x4, y4) {
     var rotate = Math.atan2(y4 - y3, x4 - x3) - Math.atan2(y2 - y1, x2 - x1),
         scale = Math.sqrt((Math.pow(y4 - y3, 2) + Math.pow(x4 - x3, 2)) / (Math.pow(y2 - y1, 2) + Math.pow(x2 - x1, 2))),
@@ -44,9 +78,20 @@ function calc(x1, y1, x2, y2, x3, y3, x4, y4) {
             [scale * Math.sin(rotate), scale * Math.cos(rotate), translate[1]],
             [0, 0, 1]
         ]
-    }
+    };
 }
 
+/**
+ * 捕获touchstart事件，将每一个新增的触点添加到gestrues
+ * 如果之前尚无被记录的触点，则绑定touchmove, touchend, touchcancel事件
+ * 
+ * 新增触点默认处于tapping状态
+ * 500毫秒之后如果还处于tapping状态，则触发press手势
+ * 如果触点数为2，则触发dualtouchstart手势，该手势的目标结点为两个触点共同的最小根结点
+ *
+ * @event
+ * @param  {event} event
+ */
 function touchstartHandler(event) {
 
     if (Object.keys(gestures).length === 0) {
@@ -55,6 +100,8 @@ function touchstartHandler(event) {
         docEl.addEventListener('touchcancel', touchcancelHandler, false);
     }
     
+    // 记录每一个触点
+    // TODO: 变量声明方式，建议在函数最前面声明
     for(var i = 0 ; i < event.changedTouches.length ; i++ ) {
         var touch = event.changedTouches[i],
             touchRecord = {};
@@ -68,6 +115,7 @@ function touchstartHandler(event) {
             startTime: Date.now(),
             status: 'tapping',
             element: event.srcElement,
+            // TODO: Don't make functions within a loop
             pressingHandler: setTimeout(function(element) {
                 return function () {
                     if (gesture.status === 'tapping') {
@@ -80,12 +128,13 @@ function touchstartHandler(event) {
 
                     clearTimeout(gesture.pressingHandler);
                     gesture.pressingHandler = null;
-                }
+                };
             }(event.srcElement), 500)
-        }
+        };
         gestures[touch.identifier] = gesture;
     }
 
+    // TODO: 变量声明方式，建议在函数最前面声明
     if (Object.keys(gestures).length == 2) {
         var elements = [];
 
@@ -100,9 +149,26 @@ function touchstartHandler(event) {
     }
 }
 
-
-
+/**
+ * 捕获touchmove事件，处理pan和dual的相关手势
+ * 
+ * 1. 遍历每个触点：
+ * > 如果触点之前处于tapping状态，且位移超过10像素，则认定为进入panning状态
+ * 先触发panstart手势，然后根据移动的方向选择性触发horizontalpanstart或verticalpanstart手势
+ * > 如果触点之前处于panning状态，则根据pan的初始方向触发horizontalpan或verticalpan手势
+ * 
+ * 2. 如果当前触点数为2，则计算出几何变换的各项参数，触发dualtouch手势
+ * 
+ * @event
+ * @param  {event} event
+ */
 function touchmoveHandler(event) {
+    // TODO: 函数太大了，影响可读性，建议分解并加必要的注释
+
+    // 遍历每个触点：
+    // 1. 如果触点之前处于tapping状态，且位移超过10像素，则认定为进入panning状态
+    // 先触发panstart手势，然后根据移动的方向选择性触发horizontalpanstart或verticalpanstart手势
+    // 2. 如果触点之前处于panning状态，则根据pan的初始方向触发horizontalpan或verticalpan手势
     for(var i = 0 ; i < event.changedTouches.length ; i++ ) {
         var touch = event.changedTouches[i],
             gesture = gestures[touch.identifier];
@@ -131,7 +197,6 @@ function touchmoveHandler(event) {
         var vx = (touch.clientX - gesture.lastTouch.clientX)/time,
             vy = (touch.clientY - gesture.lastTouch.clientY)/time;
         
-            
         var RECORD_DURATION = 70;
         if( time > RECORD_DURATION ) {
             time = RECORD_DURATION;
@@ -211,6 +276,7 @@ function touchmoveHandler(event) {
         }
     }
 
+    // 如果当前触点数为2，则计算出几何变换的各项参数，触发dualtouch手势
     if (Object.keys(gestures).length == 2) {
         var position = [],
             current = [],
@@ -218,6 +284,7 @@ function touchmoveHandler(event) {
             transform
             ;
         
+        // TODO: 变量声明方式，建议在函数最前面声明
         for(var i = 0 ; i < event.touches.length ; i++ ) {
             var touch = event.touches[i];
             var gesture = gestures[touch.identifier];
@@ -225,6 +292,7 @@ function touchmoveHandler(event) {
             current.push([touch.clientX, touch.clientY]);
         }
 
+        // TODO: 变量声明方式，建议在函数最前面声明
         for(var p in gestures) {
             elements.push(gestures[p].element);
         }
@@ -238,7 +306,23 @@ function touchmoveHandler(event) {
     }
 }
 
-
+/**
+ * 捕获touchend事件
+ * 
+ * 1. 如果当前触点数为2，则触发dualtouchend手势
+ *
+ * 2. 遍历每个触点：
+ * > 如果处于tapping状态，则触发tap手势
+ * 如果之前300毫秒出现过tap手势，则升级为doubletap手势
+ * > 如果处于panning状态，则根据滑出的速度，触发panend/flick手势
+ * flick手势被触发之后，再根据滑出的方向触发verticalflick/horizontalflick手势
+ * > 如果处于pressing状态，则触发pressend手势
+ *
+ * 3. 解绑定所有相关事件
+ * 
+ * @event
+ * @param  {event} event
+ */
 function touchendHandler(event) {
 
     if (Object.keys(gestures).length == 2) {
@@ -283,6 +367,7 @@ function touchendHandler(event) {
 
         if (gesture.status === 'panning') {
             var duration = Date.now() - gesture.startTime,
+                // TODO: velocityX & velocityY never used
                 velocityX = (touch.clientX - gesture.startTouch.clientX) / duration,
                 velocityY = (touch.clientY - gesture.startTouch.clientY) / duration,
                 displacementX = touch.clientX - gesture.startTouch.clientX,
@@ -345,7 +430,22 @@ function touchendHandler(event) {
     }
 }
 
+/**
+ * 捕获touchcancel事件
+ * 
+ * 1. 如果当前触点数为2，则触发dualtouchend手势
+ *
+ * 2. 遍历每个触点：
+ * > 如果处于panning状态，则触发panend手势
+ * > 如果处于pressing状态，则触发pressend手势
+ *
+ * 3. 解绑定所有相关事件
+ * 
+ * @event
+ * @param  {event} event
+ */
 function touchcancelHandler(event) {
+    // TODO: 和touchendHandler大量重复，建议DRY
 
     if (Object.keys(gestures).length == 2) {
         var elements = [];
@@ -394,4 +494,4 @@ function touchcancelHandler(event) {
 
 docEl.addEventListener('touchstart', touchstartHandler, false);
 
-})(window, window['lib']||(window['lib']={}))
+})(window, window.lib || (window.lib = {}));
